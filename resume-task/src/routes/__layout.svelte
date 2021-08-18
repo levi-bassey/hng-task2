47c9353cac832cc77f90427a946b58d3e233d957
Levi Bassey <programlevi@gmail.com>
2021-08-18 19:06:39 +0100

    Add skills section

--- resume-task/src/routes/__layout.svelte
+++ resume-task/src/routes/__layout.svelte
@@ -1,2 +1,9 @@
-<script>import "../app.postcss";</script>
-<slot></slot>
+<script>
+  import "../app.postcss";
+</script>
+
+<div class="h-full bg-[#282828] text-white">
+  <main>
+    <slot />
+  </main>
+</div>
--- resume-task/src/routes/index.svelte
+++ resume-task/src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import BrandIcon from "$lib/components/BrandIcon.svelte";
+
+  let skills = ["React", "Svelte", "JavaScript", "CSS", "HTML", "Typescript"];
+</script>
+
+<section class="px-6">
+  <h2 class="text-2xl">Skills</h2>
+  <ul class="mt-5 grid grid-cols-3 gap-4">
+    {#each skills as skill}
+      <li
+        class="w-20 h-20 flex flex-col items-center justify-center rounded-lg bg-[#373737]"
+      >
+        <BrandIcon --width="30px" --height="30px" name={skill.toLowerCase()} />
+        <p class="mt-2 text-sm text-[#A4A3A3]">
+          {skill}
+        </p>
+      </li>
+    {/each}
+  </ul>
+</section>

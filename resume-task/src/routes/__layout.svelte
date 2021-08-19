8f5eff559488bce25c65f56de4be310928247864
Levi Bassey <programlevi@gmail.com>
2021-08-19 16:26:56 +0100

    Make it responsive

--- resume-task/src/routes/__layout.svelte
+++ resume-task/src/routes/__layout.svelte
@@ -2,8 +2,8 @@
   import "../app.postcss";
 </script>
 
-<div class="min-h-screen bg-gray-400 text-white">
-  <main class="overflow-auto pb-20">
+<div class="min-h-screen bg-gray-400 text-white overflow-auto">
+  <main class="flex justify-center pb-20 sm:px-10 md:px-16 lg:px-32">
     <slot />
   </main>
 </div>
--- resume-task/src/routes/index.svelte
+++ resume-task/src/routes/index.svelte
@@ -44,105 +44,105 @@
   ];
 </script>
 
-<div>
-  <div class="flex flex-col items-center">
-    <div class="mt-6 w-32 h-32 rounded-full overflow-hidden">
-      <img
-        class="w-full object-cover object-center"
-        src="/sming.jpg"
-        alt="profile"
-      />
+<div
+  class="flex flex-col sm:max-w-xl sm:w-full sm:flex-row sm:justify-between md:max-w-2xl lg:max-w-4xl"
+>
+  <div class="mt-14">
+    <div class="flex flex-col items-center">
+      <div class="mt-6 w-32 h-32 rounded-full overflow-hidden">
+        <img
+          class="w-full object-cover object-center"
+          src="/sming.jpg"
+          alt="profile"
+        />
+      </div>
+      <h1 class="mt-6 text-xl font-semibold text-center">Levi Bassey</h1>
+      <p class="order-first text-2xl font-semibold text-center">
+        Frontend <br />Developer
+      </p>
+      <div class="mt-8 flex space-x-4">
+        <div class="w-5 h-5">
+          <BrandIcon name="twitter" />
+        </div>
+        <div class="w-5 h-5">
+          <BrandIcon name="github" />
+        </div>
+        <div class="w-5 h-5">
+          <BrandIcon name="linkedin" />
+        </div>
+      </div>
     </div>
-    <h1 class="mt-6 text-xl font-semibold text-center">Levi Bassey</h1>
-    <p class="order-first mt-7 text-2xl font-semibold text-center">
-      Frontend <br />Developer
-    </p>
-    <div class="mt-8 flex space-x-4">
-      <div class="w-5 h-5">
-        <BrandIcon name="twitter" />
-      </div>
-      <div class="w-5 h-5">
-        <BrandIcon name="github" />
-      </div>
-      <div class="w-5 h-5">
-        <BrandIcon name="linkedin" />
-      </div>
+  </div>
+  <div class="mt-14 px-6 flex flex-col items-center sm:px-0">
+    <div class="max-w-md w-full sm:max-w-sm md:max-w-md lg:max-w-xl">
+      <p class="text-gray-100">
+        I'm a software developer specializing in all things web. I love solving
+        problems with code and building fast and accessible web apps. I'm always
+        interested to learn about new career opportunities so please get in
+        touch with me if you would like to chat.
+      </p>
+      <section class="mt-12">
+        <h2 class="text-2xl capitalize font-semibold">Skills</h2>
+        <ul class="mt-5 grid grid-cols-auto-grid gap-3">
+          {#each skills as skill}
+            <li
+              style="padding-bottom: 100%;"
+              class="relative w-full rounded-lg bg-gray-300"
+            >
+              <div
+                class="absolute inset-0 flex flex-col items-center justify-center"
+              >
+                <div class="w-7 h-7">
+                  <BrandIcon name={skill.toLowerCase()} />
+                </div>
+                <p class="mt-2 text-sm text-gray-100 font-semibold">
+                  {skill}
+                </p>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </section>
+      <section class="mt-12">
+        <h2 class="text-2xl capitalize font-semibold">Experience</h2>
+        <ul class="mt-5 space-y-7">
+          {#each experience as { role, company, period }}
+            <li class="flex">
+              <div class="">
+                <div class="mt-2 w-2.5 h-2.5 bg-gray-100 rounded-full" />
+                <div />
+              </div>
+              <div class="ml-8">
+                <h3 class="text-lg font-semibold text-gray-100">
+                  {role}
+                </h3>
+                <p class="mt-1.5 text-gray-100">{company}</p>
+                <p class="mt-1.5 text-gray-200">{period}</p>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </section>
+      <section class="mt-12">
+        <h2 class="text-2xl capitalize font-semibold">Education</h2>
+        <ul class="mt-5 space-y-7">
+          {#each education as { course, school, period }}
+            <li class="flex">
+              <div class="">
+                <div class="mt-2 w-2.5 h-2.5 bg-gray-100 rounded-full" />
+                <div />
+              </div>
+              <div class="ml-8">
+                <h3 class="text-lg font-semibold text-gray-100">
+                  {course}
+                </h3>
+                <p class="mt-1.5 text-gray-100">{school}</p>
+                <p class="mt-1.5 text-gray-200">{period}</p>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </section>
     </div>
   </div>
 </div>
-<div class="flex justify-center px-6">
-  <p class="mt-12 max-w-md text-gray-100">
-    I'm a software developer specializing in all things web. I love solving
-    problems with code and building fast and accessible web apps. I'm always
-    interested to learn about new career opportunities so please get in touch
-    with me if you would like to chat.
-  </p>
-</div>
-<section class="flex justify-center mt-12 px-6">
-  <div class="w-full max-w-md">
-    <h2 class="text-2xl capitalize font-semibold">Skills</h2>
-    <ul class="mt-5 grid grid-cols-auto-grid gap-3">
-      {#each skills as skill}
-        <li
-          style="padding-bottom: 100%;"
-          class="relative w-full rounded-lg bg-gray-300"
-        >
-          <div
-            class="absolute inset-0 flex flex-col items-center justify-center"
-          >
-            <div class="w-7 h-7">
-              <BrandIcon name={skill.toLowerCase()} />
-            </div>
-            <p class="mt-2 text-sm text-gray-100 font-semibold">
-              {skill}
-            </p>
-          </div>
-        </li>
-      {/each}
-    </ul>
-  </div>
-</section>
-<section class="flex justify-center mt-12 px-6">
-  <div class="w-full max-w-md">
-    <h2 class="text-2xl capitalize font-semibold">Experience</h2>
-    <ul class="mt-5 space-y-7">
-      {#each experience as { role, company, period }}
-        <li class="flex">
-          <div class="">
-            <div class="mt-2 w-2.5 h-2.5 bg-gray-100 rounded-full" />
-            <div />
-          </div>
-          <div class="ml-8">
-            <h3 class="text-lg font-semibold text-gray-100">
-              {role}
-            </h3>
-            <p class="mt-1.5 text-gray-100">{company}</p>
-            <p class="mt-1.5 text-gray-200">{period}</p>
-          </div>
-        </li>
-      {/each}
-    </ul>
-  </div>
-</section>
-<section class="flex justify-center mt-12 px-6">
-  <div class="w-full max-w-md">
-    <h2 class="text-2xl capitalize font-semibold">Education</h2>
-    <ul class="mt-5 space-y-7">
-      {#each education as { course, school, period }}
-        <li class="flex">
-          <div class="">
-            <div class="mt-2 w-2.5 h-2.5 bg-gray-100 rounded-full" />
-            <div />
-          </div>
-          <div class="ml-8">
-            <h3 class="text-lg font-semibold text-gray-100">
-              {course}
-            </h3>
-            <p class="mt-1.5 text-gray-100">{school}</p>
-            <p class="mt-1.5 text-gray-200">{period}</p>
-          </div>
-        </li>
-      {/each}
-    </ul>
-  </div>
-</section>

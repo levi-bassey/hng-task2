feef464023dd5c07429ef85ac0cf0a24c49c141b
Levi Bassey <programlevi@gmail.com>
2021-08-19 08:08:00 +0100

    Add experience and education sections

--- resume-task/src/routes/__layout.svelte
+++ resume-task/src/routes/__layout.svelte
@@ -2,8 +2,8 @@
   import "../app.postcss";
 </script>
 
-<div class="h-full bg-[#282828] text-white">
-  <main>
+<div class="min-h-screen bg-[#282828] text-white">
+  <main class="overflow-auto">
     <slot />
   </main>
 </div>
--- resume-task/src/routes/index.svelte
+++ resume-task/src/routes/index.svelte
@@ -1,11 +1,40 @@
 <script>
   import BrandIcon from "$lib/components/BrandIcon.svelte";
+  import Section from "$lib/components/Section.svelte";
 
   let skills = ["React", "Svelte", "JavaScript", "CSS", "HTML", "Typescript"];
+  let experience = [
+    {
+      role: "Back-end Developer Intern",
+      company: "Javaplus Innovations",
+      period: "Jan 2019 - Jan 2020",
+    },
+    {
+      role: "Back-end Developer",
+      company: "Lumifile",
+      period: "Aug 2020 - Jun 2021",
+    },
+    {
+      role: "Front-end Developer Intern",
+      company: "HNG Internships",
+      period: "Aug 2021 - Current",
+    },
+  ];
+  let education = [
+    {
+      course: "Basic Education",
+      school: "Command Day Secondary School",
+      period: "2012 - 2015",
+    },
+    {
+      course: "Software Engineering",
+      school: "Digital bridge institute",
+      period: "2018 - Current",
+    },
+  ];
 </script>
 
-<section class="px-6">
-  <h2 class="text-2xl">Skills</h2>
+<Section heading="skills">
   <ul class="mt-5 grid grid-cols-3 gap-4">
     {#each skills as skill}
       <li
@@ -18,4 +47,42 @@
       </li>
     {/each}
   </ul>
-</section>
+</Section>
+<Section heading="experience">
+  <ul class="space-y-7">
+    {#each experience as { role, company, period }}
+      <li class="flex">
+        <div class="">
+          <div class="mt-2 w-2.5 h-2.5 bg-[#C4C4C4] rounded-full" />
+          <div />
+        </div>
+        <div class="ml-8">
+          <h3 class="text-lg font-semibold text-[#C4C4C4]">
+            {role}
+          </h3>
+          <p class="mt-1.5 text-[#C4C4C4]">{company}</p>
+          <p class="mt-1.5 text-[#7F7E7E]">{period}</p>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</Section>
+<Section heading="education">
+  <ul class="space-y-7">
+    {#each education as { course, school, period }}
+      <li class="flex">
+        <div class="">
+          <div class="mt-2 w-2.5 h-2.5 bg-[#C4C4C4] rounded-full" />
+          <div />
+        </div>
+        <div class="ml-8">
+          <h3 class="text-lg font-semibold text-[#C4C4C4]">
+            {course}
+          </h3>
+          <p class="mt-1.5 text-[#C4C4C4]">{school}</p>
+          <p class="mt-1.5 text-[#7F7E7E]">{period}</p>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</Section>

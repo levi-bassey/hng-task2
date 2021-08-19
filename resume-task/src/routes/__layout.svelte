815fd2760dae1337fa6984555e0337ab6398a029
Levi Bassey <programlevi@gmail.com>
2021-08-19 17:46:55 +0100

    add zuri logo

--- resume-task/src/routes/__layout.svelte
+++ resume-task/src/routes/__layout.svelte
@@ -3,6 +3,13 @@
 </script>
 
 <div class="min-h-screen bg-gray-400 text-white overflow-auto">
+  <header class="flex justify-center p-6 lg:px-32">
+    <div class="w-full max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-4xl">
+      <a class="font-semibold text-xl" href="https://zuri.team/"
+        >Zuri<span class="text-[red]">.</span>Team</a
+      >
+    </div>
+  </header>
   <main class="flex justify-center pb-20 sm:px-10 md:px-16 lg:px-32">
     <slot />
   </main>

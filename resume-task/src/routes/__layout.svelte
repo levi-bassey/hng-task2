e8d98b422b28bd53a4869d831827c9eafcb268e3
Levi Bassey <programlevi@gmail.com>
2021-10-08 17:29:42 +0100

    remove logo

--- resume-task/src/routes/__layout.svelte
+++ resume-task/src/routes/__layout.svelte
@@ -9,9 +9,9 @@
 <div class="min-h-screen bg-gray-400 text-white overflow-auto">
   <header class="flex justify-center p-6 lg:px-32">
     <div class="w-full max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-4xl">
-      <a class="font-semibold text-xl" href="https://zuri.team/"
-        >Zuri<span class="text-[red]">.</span>Team</a
-      >
+      <!-- <a class="font-semibold text-xl" href="https://zuri.team/"
+        >Levi<span class="text-[red]">.</span>Hackerman</a
+      > -->
     </div>
   </header>
   <main class="flex justify-center pb-20 sm:px-10 md:px-16 lg:px-32">
